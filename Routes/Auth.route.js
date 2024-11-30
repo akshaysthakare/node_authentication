9ae663cb6ea074ba3bf089f977e8edc85f44d409
akshaysthakare <thakareakshayofficial@gmail.com>
2024-12-01 00:06:19 +0530

    Added Joi validation schema to validate req body

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -11,7 +11,7 @@ router.post('/register', async (req, res, next) => {
     //if (!email || !password) throw createError.BadRequest()
 
     const result = await authSchema.validateAsync(req.body)
-    console.log(result)
+
 
     const doesExist = await User.findOne({ email: result.email })
     if (doesExist) throw createError.Conflict(`${result.email} is already registed`)
@@ -19,7 +19,7 @@ router.post('/register', async (req, res, next) => {
     const user = new User(result)
     const saveUser = await user.save()
 
-    res.send( saveUser )
+    res.send(saveUser)
 
 
   } catch (error) {
