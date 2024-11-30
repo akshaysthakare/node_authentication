5d350bb1cc2204414547682a60835d0284854e19
akshaysthakare <thakareakshayofficial@gmail.com>
2024-11-30 23:55:34 +0530

    Added Validation Schema to validate request body

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -2,23 +2,28 @@ const express = require('express')
 const router = express.Router()
 const createError = require('http-errors')
 const User = require('../Models/User.model')
+const { authSchema } = require('../helpers/validation_schema')
 
 router.post('/register', async (req, res, next) => {
   console.log(req.body)
   try {
     const { email, password } = req.body
-    if (!email || !password) throw createError.BadRequest()
+    //if (!email || !password) throw createError.BadRequest()
 
-    const doesExist = await User.findOne({ email: email })
-    if (doesExist) throw createError.Conflict(`${email} is already registed`)
+    const result = await authSchema.validateAsync(req.body)
+    console.log(result)
 
-    const user = new User({ email, password })
+    const doesExist = await User.findOne({ email: result.email })
+    if (doesExist) throw createError.Conflict(`${result.email} is already registed`)
+
+    const user = new User(result)
     const saveUser = await user.save()
 
     res.send(saveUser)
 
 
   } catch (error) {
+    if (error.isJoi === true) error.status = 422
     next(error)
   }
 })
