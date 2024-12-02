a080ad5d313ddaa3534ba1bfcf61f417061ea68a
akshaysthakare <thakareakshayofficial@gmail.com>
2024-12-02 16:49:15 +0530

    Logging in User for successfull authentication

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -30,7 +30,21 @@ router.post('/register', async (req, res, next) => {
 })
 
 router.post('/login', async (req, res, next) => {
-  res.send("login route")
+  try {
+    const result = await authSchema.validateAsync(req.body)
+    const user = await User.findOne({ email: result.email })
+    if (!user) throw createError.NotFound('User Not Registered')
+
+    const isMatch = await user.isValidPassword(result.password)
+    if (!isMatch) throw createError.Unauthorized('Email/password not valid')
+
+    const accessToken = await signAccessToken(user.id)
+
+    res.send({accessToken})
+  } catch (error) {
+    if (error.isJoi === true) return next(createError.BadRequest('Invalid Email/Password'))
+    next(error)
+  }
 })
 
 router.post('/refresh-token', async (req, res, next) => {
