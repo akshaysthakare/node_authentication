27d7bb2befe5baddd93ec359747b15a08ffcfcb9
akshaysthakare <thakareakshayofficial@gmail.com>
2024-12-02 14:55:29 +0530

    Random secret key for signing JWT

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -7,7 +7,7 @@ module.exports = {
       const payload = {
 
       }
-      const secret = "some secret";
+      const secret = process.env.ACCESS_TOKEN_SECRET;
       const options = {
         expiresIn: '1h',
         issuer: 'pickup.com',
